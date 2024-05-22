f8aad92d37df6780ffc9ced0421ef3901a134ea3
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-22 16:52:19 -0300

    feat: method one, two and three

---
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -0,0 +1,33 @@
+using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456, 7895, 4785.40, Gustavo Santos";
+
+            var encoding = Encoding.UTF8;
+
+            var bytes = encoding.GetBytes(contaComoString);
+
+            fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+
+    static void CriarArquivoComWriter()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using(var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            escritor.Write("456, 65465, 456.0, Pedro");
+        }
+    }
+
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,27 +5,8 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDeArquivo = "contas.txt";
-
-        using(var fluxoDeArquivo = new FileStream(enderecoDeArquivo, FileMode.Open))
-        {
-            var leitor = new StreamReader(fluxoDeArquivo);
-
-            // var linha = leitor.ReadLine();
-
-            // var texto = leitor.ReadToEnd();
-
-            // var numero = leitor.Read();
-
-            while(!leitor.EndOfStream)
-            {
-                var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
-            }
-
-            
-        }
-
+        CriarArquivo();
+    
         Console.ReadLine();  
     }
 
