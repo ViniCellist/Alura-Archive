c62bf60e0c46cf3f6c2915d57fd34782ed8ab0c8
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-23 14:31:52 -0300

    Update Program.cs

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -6,6 +6,9 @@ partial class Program
     static void Main(string[] args)
     {  
         EscritaBinaria();
+        LeituraBinaria();
+
+        Console.WriteLine("Aplicação Finalizada...");
 
         Console.ReadLine();  
     }
