b907322b86bd38585eb9a7d65609809988475431
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-24 12:28:04 -0300

    feat user stream

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,8 +5,7 @@ partial class Program
 {
     static void Main(string[] args)
     {  
-        EscritaBinaria();
-        LeituraBinaria();
+        UsarStreamDeEntrada();
 
         Console.WriteLine("Aplicação Finalizada...");
 
